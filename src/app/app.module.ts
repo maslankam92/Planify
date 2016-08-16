90d3b6bedc14ffbb85c00b1741be5d6f3d83d9c6
Marcin Maslanka <maslankam92@gmail.com>
2016-08-16 18:56:21 +0200

    change indentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { BrowserModule } from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/system-config.ts
+++ src/system-config.ts
@@ -8,12 +8,12 @@
  * User Configuration.
  **********************************************************************************************/
 /** Map relative paths to URLs. */
-const map: any = {
+const map:any = {
     'ng-semantic': 'vendor/ng-semantic'
 };
 
 /** User packages configuration. */
-const packages: any = {
+const packages:any = {
     'ng-semantic': {main: 'ng-semantic.js', defaultExtension: 'js'}
 };
 
@@ -21,43 +21,43 @@ const packages: any = {
 /***********************************************************************************************
  * Everything underneath this line is managed by the CLI.
  **********************************************************************************************/
-const barrels: string[] = [
-  // Angular specific barrels.
-  '@angular/core',
-  '@angular/common',
-  '@angular/compiler',
-  '@angular/forms',
-  '@angular/http',
-  '@angular/router',
-  '@angular/platform-browser',
-  '@angular/platform-browser-dynamic',
+const barrels:string[] = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/forms',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
 
-  // Thirdparty barrels.
-  'rxjs',
+    // Thirdparty barrels.
+    'rxjs',
 
-  // App specific barrels.
-  'app',
-  'app/shared',
-  /** @cli-barrel */
+    // App specific barrels.
+    'app',
+    'app/shared',
+    /** @cli-barrel */
 ];
 
-const cliSystemConfigPackages: any = {};
-barrels.forEach((barrelName: string) => {
-  cliSystemConfigPackages[barrelName] = { main: 'index' };
+const cliSystemConfigPackages:any = {};
+barrels.forEach((barrelName:string) => {
+    cliSystemConfigPackages[barrelName] = {main: 'index'};
 });
 
 /** Type declaration for ambient System. */
-declare var System: any;
+declare var System:any;
 
 // Apply the CLI SystemJS configuration.
 System.config({
-  map: {
-    '@angular': 'vendor/@angular',
-    'rxjs': 'vendor/rxjs',
-    'main': 'main.js'
-  },
-  packages: cliSystemConfigPackages
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
 });
 
 // Apply the user's configuration.
-System.config({ map, packages });
+System.config({map, packages});
