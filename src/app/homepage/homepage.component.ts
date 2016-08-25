a3ec7fa599b8067543274173cf7fb8e0c86b5732
Marcin Maslanka <mmaslanka@unity-t.pl>
2016-08-25 14:39:32 +0200

    added randomly changing background image

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../shared/auth.service";
+import { SafeStyle, DomSanitizationService } from '@angular/platform-browser';
+import { AuthService } from "../shared/auth.service";
+
 
 @Component({
   moduleId: module.id,
@@ -7,9 +9,16 @@ import {AuthService} from "../shared/auth.service";
   templateUrl: 'homepage.component.html',
   styleUrls: ['homepage.component.css']
 })
-export class HomepageComponent  {
+export class HomepageComponent implements OnInit {
+
+  images: string[] = ['bg-1.jpg', 'bg-.jpg', 'bg-3.jpg', 'bg-4.jpg', 'bg-5.jpg', 'bg-6.jpg'];
+  bg: string;
 
   constructor(private authService: AuthService) { }
+
+  ngOnInit():any {
+      this.bg = this.getBg();
+  }
 
   isAuth() {
       return this.authService.isAuthenticated();
@@ -19,5 +28,8 @@ export class HomepageComponent  {
       this.authService.signOut();
   }
 
-
+  getBg() {
+    let random = Math.floor((Math.random() * this.images.length) + 1);
+    return "url('../assets/images/bg-" + random + ".jpg')";
+  }
 }
