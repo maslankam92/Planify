f580298019f349613307004061930b312d7f0ccc
Marcin <maslankam92@gmail.com>
2016-10-10 20:59:59 +0200

    signup componnent update

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -3,16 +3,14 @@ import { UserLogin } from "./user-login.interface";
 declare var firebase: any;
 
 export class AuthService {
-    signupUser(user: UserLogin) {
-        firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
-            .catch(function(error) {
-                console.log(error);
-        });
+    signupUser(user: UserLogin): Promise<any> {
+        return firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+            .catch(this.handleError);
     }
 
     signinUser(user: UserLogin): Promise<any> {
         return firebase.auth().signInWithEmailAndPassword(user.email, user.password)
-            .then(data => data)
+            .catch(this.handleError);
     }
 
     signOut() {
@@ -28,4 +26,13 @@ export class AuthService {
             return false;
         }
     }
+
+    saveNewUser(user) {
+        return firebase.database().ref().child("users").push(user);
+    }
+
+    private handleError(error: any): Promise<any> {
+        return Promise.reject(error);
+    }
+
 }
